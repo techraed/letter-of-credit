10fb2ff3e5a9cf60176f6db539238cce55ab7b4f
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2019-08-22 22:08:01 +0300

    some new tests added

--- contracts/LetterOfCredit/README.md
+++ contracts/LetterOfCredit/README.md
@@ -8,6 +8,12 @@ should be protected from frozen states and "no respond" from the other side of a
 4. **VALIDATED** state is a signal for seller to begin shipping actions. Once seller shipped goods, shipping manager changes state to **SENT**. 
 If he does not change state to **SENT** and ```bargainDeadline``` passes, buyer will consider that as "no-respond". In order to protect his funds, he can cancel bargain.
 5. Unfortunately it's impossible to make absolutely trustless letter of credit, without involving third party. A need for a third party here is to validate that goods were shipped and received. But this "arbitier" principe widens attack opportunities. So the main "trust" point in this contract is a state between **VALIDATED** and **SENT**, because it requires a lot of off-chain actions, that can't be digitalised with smart - contracts that easily.
-6. If ```bargainDeadline``` passes when state of the contract is **SENT**, seller can protect himself with ```cancelBargainSeller``` function call. It will charge 30% of bargain to seller. The rest is transfered to buyer (LOL: seller pays gas for a transaction that transfers ether to the "off-line" buyer, it would be better to change that). Logic shows an importance of proper ```bargainDeadline``` definition. 
+6. If ```bargainDeadline``` passes when state of the contract is **SENT**, seller can protect himself with ```cancelBargainSeller``` function call. It will charge 30% of bargain to seller. The rest is transfered to buyer (LOL: seller pays gas for a transaction that transfers ether to the "off-line" buyer, it would be better to change that). Logic shows an importance of proper ```bargainDeadline``` definition. Look at **TODO** for more info concerning deadline.
 7. Buyer accepts or declines shipped goods and changes state to **ACCEPT**/**DECLINED** respectively.
 8. The final **FINISHED** state is met when call to ```transferPaymentsToParties``` is done. If bargain was declined, buyer still has to pay 15% of ```bargainSum``` as a compensation.
+
+
+## TODO:
+1. ```bargainDeadline``` should be more complicated: it should consist of ```shippingPeriod```, also ```decisionPeriod``` used to change state to **SENT** and etc.
+2. Make a proper test of ether receipt: balances before and after should be correctly compared in a view of costs of tx, send by beneficiaries.
+
---
+++ test/test_LetterOfCredit/test_cancelSellerLOC.js
@@ -0,0 +1,138 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+// .env file should contain CONTRACTS_PATH, which is ~/path/to/contracts
+
+const LetterOfCredit = artifacts.require(process.env.CONTRACTS_PATH + "LetterOfCredit/LetterOfCredit.sol");
+const { time } = require('openzeppelin-test-helpers');
+
+contract('Letter of Credit base flow', async accounts => {
+    
+    const accs = {
+        buyer: accounts[0],
+        seller: accounts[1],
+        shippingManager: accounts[2],
+        attacker: accounts[3],
+    }
+
+    let letterOfCreditContract;
+
+    let expectThrow = async(promise) => {
+        try {
+            await promise;
+        } catch (error) {
+            const invalidOpcode = error.message.search('invalid opcode') >= 0;
+            const outOfGas = error.message.search('out of gas') >= 0;
+            const revert = error.message.search('revert') >= 0;
+            assert(
+              invalidOpcode || outOfGas || revert,
+              "Expected throw, got '" + error + "' instead",
+            );
+            return;
+        }
+        assert.fail('Expected throw not received');
+    }
+    
+    before('deploying letter of credit contract', async() => {
+
+        //deploy a contract
+        letterOfCreditContract = await LetterOfCredit.new(accs.buyer, accs.seller, accs.shippingManager, { from: accs.buyer });
+        console.log('[DEBUG] Deployed LetterOfCredit contract, address: ' + letterOfCreditContract.address)
+    });
+
+    it('creating bargain', async() => {
+        let bargainSum = web3.utils.toWei("5", 'ether');
+        
+        let blocknumber = await web3.eth.getBlockNumber();
+        let block = await web3.eth.getBlock(blocknumber);
+        let bargainDeadline = block.timestamp + 1000;
+
+        //only buyer can initialize it <- that's due to how letter of credit works in real life.
+        await expectThrow(
+            letterOfCreditContract.createBargain(bargainSum, bargainDeadline, 'description', { from: accs.attacker })
+        );
+        await expectThrow(
+            letterOfCreditContract.createBargain(bargainSum, bargainDeadline, 'description', { from: accs.seller })
+        );
+        await expectThrow(
+            letterOfCreditContract.createBargain(bargainSum, bargainDeadline, 'description', { from: accs.shippingManager })
+        );
+
+        //bargain sum should be gt 0
+        await expectThrow(
+            letterOfCreditContract.createBargain(0, bargainDeadline, 'description', { from: accs.buyer })
+        );
+        await expectThrow(
+            letterOfCreditContract.createBargain(-100, bargainDeadline, 'description', { from: accs.buyer })
+        );
+
+        //msg.value should be equal to bargainSum
+        await expectThrow(
+            letterOfCreditContract.createBargain(bargainSum, bargainDeadline, 'description', { from: accs.buyer, value: web3.utils.toWei("10", "ether") })
+        );
+
+        //invalid bargain period
+        await expectThrow(
+            letterOfCreditContract.createBargain(bargainSum, bargainDeadline + 100000000000, 'description', { from: accs.buyer, value: bargainSum })
+        );
+
+        //Create valid letter of credit
+        await letterOfCreditContract.createBargain(bargainSum, bargainDeadline, 'valid bargain', { from: accs.buyer, value: bargainSum});
+        
+        // INIT state
+        let state = await letterOfCreditContract.bargainInitializedBy.call(accs.buyer);
+        assert.equal(state.bargainState.valueOf(), 1);
+    });
+
+    it('buyer gets considired that seller could be trusted and changes state', async() => {
+        /*
+         Once a bargain created and contract state is set to INIT, buyer should consider that seller could be trusted, he has a wanted product/service. 
+         When it's done, buyer changes state to VALIDATED. If doubts about seller occure, buyer can call cancelBargainBuyer and get his ether back.        
+         For the base flow we would not test bargain cancellation.
+         */
+        
+        // uint8 for State type in LetterOfCredit
+        let VALIDATED = 2;
+        let anyNonValidState = 5;
+
+        await expectThrow(letterOfCreditContract.pushStateForwardTo(anyNonValidState, { from: accs.buyer }));
+        await expectThrow(letterOfCreditContract.pushStateForwardTo(VALIDATED, { from: accs.attacker }));
+
+        await letterOfCreditContract.pushStateForwardTo(VALIDATED, { from: accs.buyer });
+        let state = await letterOfCreditContract.bargainInitializedBy.call(accs.buyer);
+        assert.equal(state.bargainState.valueOf(), VALIDATED);
+    });
+
+    it('seller sends goods, shipping manager changes state', async() => {
+        // Please read the 4th paragraph in README.md
+        let SENT = 3;
+        let anyNonValidState = 5;
+
+        //invalid access
+        await expectThrow(letterOfCreditContract.pushStateForwardTo(SENT, { from: accs.attacker }));
+
+        //wrong state transition test
+        await expectThrow(letterOfCreditContract.pushStateForwardTo(anyNonValidState, { from: accs.shippingManager }));
+
+        await letterOfCreditContract.pushStateForwardTo(SENT, { from: accs.shippingManager });
+        let state = await letterOfCreditContract.bargainInitializedBy.call(accs.buyer);
+        assert.equal(state.bargainState.valueOf(), SENT);
+    });
+
+    it('no respond from buyer, seller cancels bargain', async() => {
+        let ZS = 0;
+        //period didn't expired
+        await expectThrow(letterOfCreditContract.cancelBargainSeller({ from: accs.attacker }));
+
+        // Increasing time
+        await time.advanceBlock();
+        let start = await time.latest();
+        let end = start.add(time.duration.years(2));
+        await time.increaseTo(end);
+
+        await letterOfCreditContract.cancelBargainSeller({ from: accs.seller });
+        let state = await letterOfCreditContract.bargainInitializedBy.call(accs.buyer);
+
+        assert.equal(state.bargainState.valueOf(), ZS);
+    });
+})
